c09866f7c1398492880cdd060fd479b55c361c7b
Akonicki26 <anne.konicki@mymail.champlain.edu>
2026-05-15 17:56:24 -0400

    Add worker for engine

--- src/tts_engine/tts_engine.rs
+++ src/tts_engine/tts_engine.rs
@@ -8,15 +8,63 @@ use tts::Tts;
 use crate::tts_engine::key_listener::KeyListener;
 use crate::tts_engine::tts_key::TtsKey;
 
-pub struct TtsEngine {
+struct TtsEngineWorker {
     hotkeys: HashSet<TtsKey>,
     tts: Tts,
+}
+
+impl TtsEngineWorker {
+    fn run(mut self, key_rx: Receiver<Event>, bind_rx: Receiver<(Key, String)>) {
+        println!("Event loop started");
+        loop {
+            self.process_bind_queue(&bind_rx);
+
+            match key_rx.recv_timeout(Duration::from_millis(10)) {
+                Ok(event) => {
+                    if let EventType::KeyPress(key) = event.event_type {
+                        self.handle_keypress(key);
+                    }
+                }
+                Err(mpsc::RecvTimeoutError::Timeout) => {}
+                Err(mpsc::RecvTimeoutError::Disconnected) => break,
+            }
+        }
+    }
+
+    fn process_bind_queue(&mut self, bind_rx: &Receiver<(Key, String)>) {
+        while let Ok((key, output)) = bind_rx.try_recv() {
+            self.hotkeys.retain(|hotkey| !hotkey.is_same_key(key));
+            if !output.is_empty() {
+                self.hotkeys.insert(TtsKey::from_string(key, output));
+                println!("Added keybind: {:?}", key);
+            }
+        }
+    }
+
+    fn handle_keypress(&mut self, key: Key) {
+        let message = self.hotkeys.iter()
+            .find(|hotkey| hotkey.is_same_key(key))
+            .map(|hotkey| hotkey.get_output());
+
+        if let Some(msg) = message {
+            println!("Hotkey Pressed: {:?}", key);
+            self.tts.speak(&msg, true).ok();
+        }
+    }
+}
+
+pub struct TtsEngine {
+    worker: Option<TtsEngineWorker>,
     keybind_sender: Option<Sender<(Key, String)>>,
     thread_handle: Option<JoinHandle<()>>,
+    speaking_sender: Option<Sender<bool>>,
+    can_speak: bool,
 }
 
 impl TtsEngine {
     pub fn start(&mut self) {
+        let worker = self.worker.take().expect("Engine already started");
+
         let (key_tx, key_rx) = mpsc::channel::<Event>();
         let (bind_tx, bind_rx) = mpsc::channel::<(Key, String)>();
 
@@ -28,14 +76,8 @@ impl TtsEngine {
             });
         });
 
-        let hotkeys = std::mem::take(&mut self.hotkeys);
-        let tts = std::mem::replace(
-            &mut self.tts,
-            Tts::default().expect("Error when creating TTS engine"),
-        );
-
         self.thread_handle = Some(std::thread::spawn(move || {
-            Self::run_event_loop(key_rx, bind_rx, hotkeys, tts);
+            worker.run(key_rx, bind_rx);
         }));
     }
 
@@ -55,8 +97,7 @@ impl TtsEngine {
     pub fn modify_keybinding(&mut self, key: Key, output: String) {
         match &self.keybind_sender {
             Some(sender) => { let _ = sender.send((key, output)); }
-            None => { panic!("Cannot modify keybindings before starting the engine");
-            }
+            None => { panic!("Cannot modify keybindings before starting the engine"); }
         }
     }
 
@@ -67,67 +108,29 @@ impl TtsEngine {
         }
     }
 
-    fn run_event_loop(
-        key_rx: Receiver<Event>,
-        bind_rx: Receiver<(Key, String)>,
-        mut hotkeys: HashSet<TtsKey>,
-        mut tts: Tts,
-    ) {
-        println!("Event loop started");
-        loop {
-            // add keybinds to the system if any are requested
-            Self::process_bind_queue(&bind_rx, &mut hotkeys);
-
-            // check for key presses
-            match key_rx.recv_timeout(Duration::from_millis(10)) {
-                // if a key press is detected, handle it
-                Ok(event) => {
-                    if let EventType::KeyPress(key) = event.event_type {
-                        Self::handle_keypress(key, &hotkeys, &mut tts);
-                    }
-                }
-                // if we errored for some reason, continue or exit
-                Err(mpsc::RecvTimeoutError::Timeout) => {}
-                Err(mpsc::RecvTimeoutError::Disconnected) => break,
-            }
+    pub fn allow_speaking(&mut self, allow: bool) {
+        match &self.speaking_sender {
+            Some(sender) => { let _ = sender.send(allow); }
+            None => { panic!("Cannot allow speaking before starting the engine"); }
         }
     }
 
-    fn process_bind_queue(bind_rx: &Receiver<(Key, String)>, hotkeys: &mut HashSet<TtsKey>) {
-        while let Ok((key, output)) = bind_rx.try_recv() {
-            // clear any existing keybinds for this key
-            hotkeys.retain(|hotkey| !hotkey.is_same_key(key));
-
-            // if the output is empty, return early since we don't need to add anything
-            if output.is_empty() {
-                continue;
-            }
-
-            // add the new keybind
-            hotkeys.insert(TtsKey::from_string(key, output));
-            println!("Added keybind: {:?}", key);
-        }
-    }
-
-    fn handle_keypress(key: Key, hotkeys: &HashSet<TtsKey>, tts: &mut Tts) {
-        let message = hotkeys.iter()
-            .find(|hotkey| hotkey.is_same_key(key))
-            .map(|hotkey| hotkey.get_output());
-
-        if let Some(msg) = message {
-            println!("Hotkey Pressed: {:?}", key);
-            tts.speak(msg, true).ok();
-        }
+    pub fn block_speaking(&mut self) {
+        self.can_speak = false;
     }
 }
 
 impl Default for TtsEngine {
     fn default() -> Self {
         Self {
-            hotkeys: HashSet::new(),
-            tts: Tts::default().expect("Error when creating TTS engine"),
+            worker: Some(TtsEngineWorker {
+                hotkeys: HashSet::new(),
+                tts: Tts::default().expect("Error when creating TTS engine"),
+            }),
             keybind_sender: None,
             thread_handle: None,
+            speaking_sender: None,
+            can_speak: true,
         }
     }
 }
