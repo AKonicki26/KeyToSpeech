62d030d8d2f8c5db4ed2f0f9cb051b811ac71b68
Akonicki26 <anne.konicki@mymail.champlain.edu>
2026-07-05 16:19:48 -0400

    Change TtsVoice to auto property

--- Tts/TtsVoice.cs
+++ Tts/TtsVoice.cs
@@ -2,15 +2,9 @@
 
 public class TtsVoice(string name, string voiceKey, int pitch, int rate, int range)
 {
-    private string _name = name;
-    private string _voiceKey = voiceKey;
-    private int _pitch = pitch;
-    private int _rate = rate;
-    private int _range = range;
-    
-    public string Name  { get => _name; set => _name = value; }
-    public string VoiceKey { get => _voiceKey; set => _voiceKey = value; }
-    public int Pitch { get => _pitch; set => _pitch = value; }
-    public int Rate { get => _rate; set => _rate = value; }
-    public int Range { get => _range; set => _range = value; }
+    public string Name { get; set; } = name;
+    public string VoiceKey { get; set; } = voiceKey;
+    public int Pitch { get; set; } = pitch;
+    public int Rate { get; set; } = rate;
+    public int Range { get; set; } = range;
 }
