258bdf4f2f7ba94ecd4abf755dc0174254f5aea3
Akonicki26 <anne.konicki@mymail.champlain.edu>
2026-07-15 22:51:25 -0400

    proof of concept of tomodachi version
    This is THE version going forward. Just need to make it good

---
+++ Tts/TomodachiAPI/TomodachiAPI.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Media;
+
+namespace Tts.TomodachiAPI;
+
+public static class TomodachiAPI
+{
+    
+    private static readonly string apiPath = "https://talkmodachi.dylanpdx.io/tts";
+    private static HttpClient client = new();
+    
+    public static async Task Main()
+    {
+
+        while (true)
+        {
+            var message = Console.ReadLine();
+            if (message == "exit") 
+            {
+                break;
+            }
+            
+            GetSoundAndPlayIt(message);
+        }
+
+        Console.WriteLine("Shutting down...");
+        
+    }
+
+
+    private static async Task GetSoundAndPlayIt(string message)
+    {
+        var requestPayload = new TomodachiApiArgs()
+        {
+            Text = message,
+            Pitch = 75,
+        };
+        
+        var jsonBody = requestPayload.SerializeToJson();
+
+        using (var request = new HttpRequestMessage(HttpMethod.Get, $"{apiPath}?{requestPayload.ApiParamString}"))
+        {
+            try
+            {
+                using (HttpResponseMessage response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    Debug.WriteLine("Successful response:");
+
+                    using (FileStream diskStream = new FileStream("./temp.wav", FileMode.Create, FileAccess.ReadWrite,
+                        FileShare.Read))
+                    {
+                        await response.Content.CopyToAsync(diskStream);
+                        
+                        Debug.WriteLine("File saved successfully");
+                        Debug.WriteLine($"File location: {diskStream.Name}");
+                        
+// TODO: Move to cross platform API or just make people use Wine.
+// Prolly dev on Windows and force people to suck it up on wine if they use linux
+#pragma warning disable CA1416
+                        
+                        
+                        
+                        SoundPlayer player = new SoundPlayer(diskStream.Name);
+
+                        try
+                        {
+                            Debug.WriteLine("Playing sound...");
+                            player.LoadAsync();
+                            
+                            player.PlaySync();
+                        }
+                        catch (FileNotFoundException exception)
+                        {
+                            // The file specified by 'P:System.Media.SoundPlayer.SoundLocation' cannot be found.
+                            Console.WriteLine(exception.Message);
+                        }
+#pragma warning restore CA1416
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+            }
+        }
+    }
+    
+    
+    
+}
---
+++ Tts/TomodachiAPI/TomodachiLanguage.cs
@@ -0,0 +1,19 @@
+﻿namespace Tts.TomodachiAPI;
+
+public class TomodachiLanguage
+{
+    public string LanguageCode { get; }
+    
+    private TomodachiLanguage(string languageCode) {  LanguageCode = languageCode; }
+
+    public static TomodachiLanguage UnitedStatesEnglish { get; } = new("useng");
+    public static TomodachiLanguage EuropeEnglish { get; } = new("eueng");
+    public static TomodachiLanguage Spanish { get; } = new("es");
+    public static TomodachiLanguage German { get; } = new("de");
+    public static TomodachiLanguage French { get; } = new("fr");
+    public static TomodachiLanguage Italian { get; } = new("it");
+    public static TomodachiLanguage Japanese { get; } = new("jp");
+    public static TomodachiLanguage Korean { get; } = new("kr");
+
+    public override string ToString() => LanguageCode;
+}
