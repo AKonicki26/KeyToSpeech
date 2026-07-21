cc5b00a261aa7889219e46cb25ce8964c174bffc
AKonicki26 <astrid.konicki@mymail.champlain.edu>
2026-07-20 21:11:20 -0400

    Add support in TtsEngine for linux

--- Tts/TomodachiAPI/TomodachiTtsEngine.cs
+++ Tts/TomodachiAPI/TomodachiTtsEngine.cs
@@ -8,8 +8,26 @@ namespace Tts.TomodachiAPI;
 internal static class TomodachiTtsEngine
 {
     private static readonly string TomodachiApiUrl = "https://talkmodachi.dylanpdx.io/tts";
-    private static HttpClient _client = new HttpClient();
+    private static readonly HttpClient _client = new HttpClient();
     private static SoundPlayer? _soundPlayer = null;
+
+    /// <summary>
+    /// Holds the operating system specific function to play the sound.
+    /// </summary>
+    private static readonly Func<string, Task> PlaySoundDelegate;
+
+    static TomodachiTtsEngine()
+    {
+        // Assign the delegate for playing the wav audio file based on operating system
+        PlaySoundDelegate = true switch
+        {
+            _ when OperatingSystem.IsWindows() => PlaySoundWindows,
+            _ when OperatingSystem.IsLinux() => PlaySoundLinux,
+            _ when OperatingSystem.IsMacOS() => PlaySoundMacOS,
+            _ => throw new Exception(
+                "Unsupported OS. KeyToSpeech only supports Windows, Linux, and MacOS at this time.")
+        };
+    }
     
     private static string GetApiParams(string message, TomodachiVoice voice) =>
         $"text={message.Replace(" ", "+")}&" +
@@ -46,23 +64,13 @@ internal static class TomodachiTtsEngine
     }
 
 // disable warning since SoundPlayer is Windows only
-#pragma warning disable CA1416 
+#pragma warning disable CA1416
     internal static async Task PlaySound(string fileLocation)
     {
         Debug.WriteLine($"Playing {fileLocation}");
-        using (var outputDevice = new WaveOutEvent())
-        using (var audioFile = new AudioFileReader(fileLocation))
-        {
-            outputDevice.Init(audioFile);
 
-            outputDevice.Play();
-
-            // while we are playing, sleep fo
-            while (outputDevice.PlaybackState == PlaybackState.Playing)
-            {
-                Thread.Sleep(10);
-            }
-        }
+        // Call the operating system specific function to play the sound
+        await PlaySoundDelegate(fileLocation);
     }
     
     internal static async Task PlaySound(string fileLocation, SoundPlayer? player)
@@ -82,4 +90,34 @@ internal static class TomodachiTtsEngine
         player.PlaySync();
     }
 #pragma warning restore CA1416
+
+
+    private static async Task PlaySoundWindows(string fileLocation)
+    {
+        using var outputDevice = new WaveOutEvent();
+        using var audioFile = new AudioFileReader(fileLocation);
+
+        outputDevice.Init(audioFile);
+        outputDevice.Play();
+
+        // while we are playing, sleep fo
+        while (outputDevice.PlaybackState == PlaybackState.Playing)
+        {
+            Thread.Sleep(10);
+        }
+    }
+
+    private static async Task PlaySoundLinux(string fileLocation)
+    {
+        const string playerProcessName = "pw-play";
+        using var process = Process.Start(new ProcessStartInfo(playerProcessName, fileLocation) { UseShellExecute = false });
+        await process!.WaitForExitAsync();
+    }
+
+    private static async Task PlaySoundMacOS(string fileLocation)
+    {
+        const string playerProcessName = "afplay";
+        using var process = Process.Start(new ProcessStartInfo(playerProcessName, fileLocation) { UseShellExecute = false });
+        await process!.WaitForExitAsync();
+    }
 }
